840c7d26c865418aa98a0e7a8d330048b0cd0f9f
Sirijatinnavalli <109924029+Sirijatinnavalli@users.noreply.github.com>
2024-03-15 15:51:36 +0530

    Add files via upload

---
+++ metrics.js
@@ -0,0 +1,15 @@
+exports.kmtom=function(x){
+    return 1000*x;
+};
+exports.mtokm=function(x){
+    return 0.001*x;
+};
+exports.mtocm=function(x){
+    return 100*x;
+};
+exports.cmtom=function(x){
+    return 0.01*x;
+};
+exports.cmtomm=function(x){
+    return 10*x;
+};
